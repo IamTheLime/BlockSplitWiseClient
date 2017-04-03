2cd0ba52c8e610e12d7fb1089563593003ec9ca3
IAmTheLime <a70672@alunos.uminho.pt>
2017-04-03 21:46:36 +0100

    Está mais bonito

---
+++ ClientBlockSplitWise/app/src/main/java/com/blocksplitwise/clientblocksplitwise/Friends.java
@@ -0,0 +1,47 @@
+package com.blocksplitwise.clientblocksplitwise;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.NavUtils;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class Friends extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window w = getWindow();
+            w.addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+            w.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        }
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friends);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        findViewById(R.id.toolbar).setPadding(0,50,0,0);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent returnIntent = new Intent();
+        //returnIntent.putExtra("result",result);
+        //Pode ser activity.result_OK
+        setResult(Activity.RESULT_CANCELED,returnIntent);
+        finish();
+        return true;
+    }
+}
---
+++ ClientBlockSplitWise/app/src/main/java/com/blocksplitwise/clientblocksplitwise/Groups.java
@@ -0,0 +1,43 @@
+package com.blocksplitwise.clientblocksplitwise;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class Groups extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window w = getWindow();
+            w.addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+            w.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        }
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_groups);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        findViewById(R.id.toolbar).setPadding(0,50,0,0);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent returnIntent = new Intent();
+        //returnIntent.putExtra("result",result);
+        setResult(Activity.RESULT_CANCELED,returnIntent);
+        finish();
+        return true;
+    }
+
+}
