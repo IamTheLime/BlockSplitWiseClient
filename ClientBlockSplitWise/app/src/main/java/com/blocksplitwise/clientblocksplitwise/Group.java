82613e99c6befb6453168cdbc995dec79491c189
IAmTheLime <a70672@alunos.uminho.pt>
2017-05-13 13:54:31 +0100

    Adicionados novos Assets

--- ClientBlockSplitWise/app/src/main/java/com/blocksplitwise/clientblocksplitwise/Group.java
+++ ClientBlockSplitWise/app/src/main/java/com/blocksplitwise/clientblocksplitwise/Group.java
@@ -1,23 +1,13 @@
 package com.blocksplitwise.clientblocksplitwise;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Build;
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.NavUtils;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
-import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
-public class Friends extends AppCompatActivity {
+public class Group extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,22 +17,11 @@ public class Friends extends AppCompatActivity {
             w.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
         }
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_friends);
+        setContentView(R.layout.activity_group);
+        setContentView(R.layout.activity_groups);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         findViewById(R.id.toolbar).setPadding(0,50,0,0);
-
     }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        Intent returnIntent = new Intent();
-        //returnIntent.putExtra("result",result);
-        //Pode ser activity.result_OK
-        setResult(Activity.RESULT_CANCELED,returnIntent);
-        finish();
-        return true;
-    }
-
 }
--- ClientBlockSplitWise/app/src/main/java/com/blocksplitwise/clientblocksplitwise/Groups.java
+++ ClientBlockSplitWise/app/src/main/java/com/blocksplitwise/clientblocksplitwise/Groups.java
@@ -2,19 +2,32 @@ package com.blocksplitwise.clientblocksplitwise;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.media.Image;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.PopupMenu;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageButton;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 
 public class Groups extends AppCompatActivity {
-
+    private CardView cardView;
+    private CardView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
@@ -28,7 +41,19 @@ public class Groups extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         findViewById(R.id.toolbar).setPadding(0,50,0,0);
+        final ImageButton buttonArrow;
+        //Creates popup button whenever the arrow is pressed, Still A WIP
+        buttonArrow = (ImageButton) findViewById(R.id.imageButton);
+        buttonArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupMenu popup = new PopupMenu(Groups.this,buttonArrow, Gravity.RIGHT);
+                popup.getMenuInflater().inflate(R.menu.popup_menu,popup.getMenu());
+                popup.show();
+            }
 
+        });
+        //Code to Implement the scrollable groups
     }
 
     @Override
@@ -39,5 +64,7 @@ public class Groups extends AppCompatActivity {
         finish();
         return true;
     }
+
+
 
 }
